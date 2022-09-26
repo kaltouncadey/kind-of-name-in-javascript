9a162bb25d7b7d5849b4939289258073e4d9b6ab
kaltouncadey <kaltuunaadam35@gmail.com>
2022-09-26 04:47:08 -0700

    first name

---
+++ app.js
@@ -0,0 +1,8 @@
+let name1 = "asma";
+let name2 = "ahmed";
+// console.log(name1,name2);
+
+const magc1 = "yasin";
+const magc2 = "ahmed";
+const fullname = magc1 + '' + magc2
+console.log(magc1,magc2)
